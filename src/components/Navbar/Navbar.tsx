6ca565408b4b2accc02da2d95c9df36e506fa475
Tim Quattrochi <timq82@gmail.com>
2024-03-19 14:06:28 -0400

    initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <h2>BEACHES.</h2>
+      </div>
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Books</li>
+        <li>Views</li>
+      </ul>
+    </div>
+  );
+}
+export default Navbar;
